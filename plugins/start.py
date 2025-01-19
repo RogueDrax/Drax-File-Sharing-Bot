07671320d67cf4446926d39dd8bfce2837eb6c2e
RogueDrax <rogue.draxx@gmail.com>
2025-01-19 20:19:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -112,20 +112,6 @@ async def start_command(client: Client, message: Message):
             ]]
         )
 
-        await message.reply_photo(
-          photo="https://ibb.co/dKcJ1W0",  # Replace with the image URL or file path
-             caption=START_MSG.format(
-             first=message.from_user.first_name,
-             last=message.from_user.last_name,
-             username=None if not message.from_user.username else '@' + message.from_user.username,
-             mention=message.from_user.mention,
-             id=message.from_user.id
-        ),
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-        )
-
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
