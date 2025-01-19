7ffa54dcc7f56c654ecfb20625944e863a830882
RogueDrax <rogue.draxx@gmail.com>
2025-01-19 12:46:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -125,6 +125,15 @@ async def start_command(client: Client, message: Message):
         )
         return
 
+    reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+        return
+
     
 
 
