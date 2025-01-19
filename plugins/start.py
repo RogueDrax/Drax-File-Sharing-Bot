a1c13225bfadb5c14a1b0ef98b02ac03c85a3f31
RogueDrax <rogue.draxx@gmail.com>
2025-01-19 12:46:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -125,21 +125,9 @@ async def start_command(client: Client, message: Message):
         )
         return
 
-    reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
-        return
-
-    
-
-
-
     
     
+
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
