f0dee6d8ab1e287d18e62453c769bf4e7021987b
RogueDrax <rogue.draxx@gmail.com>
2025-01-19 20:36:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -113,8 +113,8 @@ async def start_command(client: Client, message: Message):
         )
 
         await message.reply_text(
-            await message.reply_photo(f"https://ibb.co/dKcJ1W0",
             text = START_MSG.format(
+                await message.reply_photo(f"https://ibb.co/dKcJ1W0",
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
@@ -124,6 +124,7 @@ async def start_command(client: Client, message: Message):
             reply_markup = reply_markup,
             disable_web_page_preview = True,
             quote = True
+        
         )
         )
         return
